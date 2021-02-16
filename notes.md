ddc218f8ad207d0a7c8b88cb82aef7466ffdc025
Emanuele Pace <emanuele.10@outlook.it>
2021-02-16 15:18:39 +0000

    Added funcionality to count parmeter longer than one letter

---
+++ notes.md
@@ -0,0 +1,7 @@
+I can acces element inside a object in 2 ways:
+ let values = {
+... "a":1,
+... "f":4
+... }
+1. values.a --> 1
+2. values['a'] --> 1
--- scrabble.js
+++ scrabble.js
@@ -2,16 +2,51 @@ class Scrabble{
     
     constructor(string){
         this.string = string
+        this.value = {
+            'a': 1,
+            'b': 3,
+            'c': 3,
+            'd': 2,
+            'e': 1,
+            'f': 4,
+            'g': 2,
+            'h': 4,
+            'i': 1,
+            'j': 8,
+            'k': 5,
+            'l': 1,
+            'm': 3,
+            'n': 1,
+            'o': 1,
+            'p': 3,
+            'q': 10,
+            'r': 1,
+            's': 1,
+            't': 1,
+            'u': 1,
+            'v': 4,
+            'w': 4,
+            'x': 8,
+            'y': 4,
+            'z':10 
+        }
     };
 
     score(){
+        let array = []
         if ((this.string == "") || (this.string == '/\t\n/') || (this.string === null)){
             return 0
-        } else if (this.string === "a"){
-            return 1
-        } else if (this.string === "f") {
-            return 4
+        } else {
+            for(let i = 0; i < this.string.length; i++ ){
+                let letter = this.string[i] //a
+                array.push(this.value[letter])
+            }
+            let result = array.reduce(function(a,b){
+                return a + b;
+            } ,0)
+            return result
         }
+        
     }
     
 }
